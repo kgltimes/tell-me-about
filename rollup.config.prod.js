f8032f8eea7ba286bbb114854c6a90a83d95d0e6
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-06-25 13:21:43 -0400

    use different rollup config files

---
+++ rollup.config.prod.js
@@ -0,0 +1,21 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+export default {
+  entry: 'es6/index.js',
+  format: 'cjs',
+  sourceMap: false,  
+  external: ['node-fetch'],
+  plugins: [
+    resolve({jsnext: true}),
+    sourcemaps(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    uglify()
+  ],
+  dest: 'es5/index.js'
+};
